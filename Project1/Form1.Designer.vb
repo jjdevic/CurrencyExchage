f22b9e8f0b68a46dddb963ac20f0199da539b86a
jjdevic <jdevic01@ucm.es>
2023-06-19 15:10:25 +0200

    Agregar archivos de proyecto.

---
+++ Project1/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.DarkGray
+        Me.Button1.Font = New System.Drawing.Font("Gadugi", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(650, 98)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(150, 61)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Convert"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.DarkGray
+        Me.TextBox1.Font = New System.Drawing.Font("Gadugi", 10.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(50, 110)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox1.MaxLength = 8
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(150, 46)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.BackColor = System.Drawing.Color.LightGray
+        Me.ComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(250, 110)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(150, 32)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.BackColor = System.Drawing.Color.LightGray
+        Me.ComboBox2.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(450, 110)
+        Me.ComboBox2.Margin = New System.Windows.Forms.Padding(2)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(150, 32)
+        Me.ComboBox2.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Gadugi", 16.125!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FloralWhite
+        Me.Label1.Location = New System.Drawing.Point(261, 180)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 51)
+        Me.Label1.TabIndex = 4
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Gadugi", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FloralWhite
+        Me.Label2.Location = New System.Drawing.Point(279, 55)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 39)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "From"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Gadugi", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.FloralWhite
+        Me.Label3.Location = New System.Drawing.Point(496, 55)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 39)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "To"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(850, 300)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Currency Exchange"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Project1/Form1.vb
@@ -0,0 +1,156 @@
+﻿Public Class Form1
+    Dim conv1, conv2 As Double
+    Dim nameAux As String
+    ReadOnly textAux As String = "1"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim currency As New List(Of Item)
+        Dim currency2 As New List(Of Item)
+
+        ComboBox1.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox2.DropDownStyle = ComboBoxStyle.DropDownList
+
+        AddList(currency, currency2)
+
+        ComboBox1.DataSource = currency
+        ComboBox2.DataSource = currency2
+
+        Aesthetics()
+    End Sub
+
+    Private Sub Aesthetics()
+        TextBox1.Text = textAux
+        Me.BackgroundImage = My.Resources.gris2
+        Me.BackgroundImageLayout = ImageLayout.Stretch
+
+        Label1.BackColor = Color.Transparent
+        Label2.BackColor = Color.Transparent
+        Label3.BackColor = Color.Transparent
+
+        TextBox1.BorderStyle = BorderStyle.None
+
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.FlatAppearance.BorderSize = 0
+
+        ComboBox1.FlatStyle = FlatStyle.Flat
+        ComboBox2.FlatStyle = FlatStyle.Flat
+
+        Label1.Text = "1 USD"
+    End Sub
+
+    Private Sub AddList(currency As List(Of Item), currency2 As List(Of Item))
+        currency.Add(New Item(prize:=1, id:="USD"))
+        currency.Add(New Item(prize:=0.9135, id:="EUR"))
+        currency.Add(New Item(prize:=56.2983, id:="MKD"))
+        currency.Add(New Item(prize:=107.1508, id:="RSD"))
+        currency.Add(New Item(prize:=98.7278, id:="ALL"))
+        currency.Add(New Item(prize:=1.7867, id:="BAM"))
+        currency.Add(New Item(prize:=1.7867, id:="BNG"))
+        currency.Add(New Item(prize:=4.5311, id:="RON"))
+        currency.Add(New Item(prize:=0.8939, id:="CHF"))
+        currency.Add(New Item(prize:=10.643, id:="SEK"))
+
+        currency2.Add(New Item(prize:=1, id:="USD"))
+        currency2.Add(New Item(prize:=0.9135, id:="EUR"))
+        currency2.Add(New Item(prize:=56.2983, id:="MKD"))
+        currency2.Add(New Item(prize:=107.1508, id:="RSD"))
+        currency2.Add(New Item(prize:=98.7278, id:="ALL"))
+        currency2.Add(New Item(prize:=1.7867, id:="BAM"))
+        currency2.Add(New Item(prize:=1.7867, id:="BNG"))
+        currency2.Add(New Item(prize:=4.5311, id:="RON"))
+        currency2.Add(New Item(prize:=0.8939, id:="CHF"))
+        currency2.Add(New Item(prize:=10.643, id:="SEK"))
+    End Sub
+
+    Private Sub ComboBox1_DrawItem(sender As Object, e As DrawItemEventArgs) Handles ComboBox1.DrawItem
+        If e.Index >= 0 Then
+            Dim texto As String = ComboBox1.Items(e.Index).ToString()
+            Dim flag As Image = My.Resources.ResourceManager.GetObject(texto.ToLower)
+            Dim resizedFlag As New Bitmap(flag, 22, 15)
+
+            'e.DrawBackground()
+            e.Graphics.DrawImage(resizedFlag, e.Bounds.Left, e.Bounds.Top)
+            e.Graphics.DrawString(texto, ComboBox1.Font, Brushes.Black, e.Bounds.Left + resizedFlag.Width, e.Bounds.Top + (e.Bounds.Height - ComboBox1.Font.Height) / 2)
+        End If
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Dim aux As Item = ComboBox1.SelectedItem
+        conv1 = aux.Prize
+    End Sub
+
+    Private Sub ComboBox2_DrawItem(sender As Object, e As DrawItemEventArgs) Handles ComboBox2.DrawItem
+        If e.Index >= 0 Then
+            Dim texto As String = ComboBox2.Items(e.Index).ToString()
+            Dim flag As Image = My.Resources.ResourceManager.GetObject(texto.ToLower)
+            Dim resizedFlag As New Bitmap(flag, 22, 15)
+
+            'e.DrawBackground()
+            e.Graphics.DrawImage(resizedFlag, e.Bounds.Left, e.Bounds.Top)
+            e.Graphics.DrawString(texto, ComboBox2.Font, Brushes.Black, e.Bounds.Left + resizedFlag.Width, e.Bounds.Top + (e.Bounds.Height - ComboBox2.Font.Height) / 2)
+        End If
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        Dim aux As Item = ComboBox2.SelectedItem
+        conv2 = aux.Prize
+        nameAux = aux.Id
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num As Integer = Val(TextBox1.Text)
+        Dim result As Double = (num / conv1) * conv2
+        Dim text As String = result.ToString
+
+        If result.ToString.Contains(",") Then
+            Dim index As Integer = text.IndexOf(",")
+            Label1.Text = text.Substring(0, index + 3)
+        ElseIf text.Length >= 5 Then
+            Label1.Text = text.Substring(0, 5)
+        Else
+            Label1.Text = text
+        End If
+        Label1.Text = Label1.Text & " " & nameAux
+
+    End Sub
+    Private Sub TextBox1_GotFocus(sender As Object, e As EventArgs) Handles TextBox1.GotFocus
+        If TextBox1.Text = textAux Then
+            TextBox1.Text = ""
+        End If
+    End Sub
+
+    Private Sub TextBox1_LostFocus(sender As Object, e As EventArgs) Handles TextBox1.LostFocus
+        If String.IsNullOrWhiteSpace(TextBox1.Text) Then
+            TextBox1.Text = textAux
+        End If
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub TextBox1_Click(sender As Object, e As EventArgs) Handles TextBox1.Click
+        TextBox1.Text = String.Empty
+    End Sub
+
+    Private Sub Label1_SizeChanged(sender As Object, e As EventArgs) Handles Label1.SizeChanged
+        Label1.Left = (Me.ClientSize.Width - Label1.Width) \ 2
+        Label1.Top = ((Me.ClientSize.Height - Label1.Height) \ 2) + 35
+    End Sub
+
+End Class
+
+Public Class Item
+    Public Property Prize As Double
+    Public Property Id As String
+
+    Public Sub New(id As String, prize As Double)
+        Me.Prize = prize
+        Me.Id = id
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return Id
+    End Function
+End Class
